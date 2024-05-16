6815680e8b55c15106fba9f00f25ba18d03df22e
K0ndrich <123kondrich@gmail.com>
2024-05-16 17:25:55 +0300

    edited

--- send_email/main/views.py
+++ send_email/main/views.py
@@ -1,3 +1,20 @@
+from django.forms import BaseModelForm
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
 
-# Create your views here.
+from .models import Contact
+from .forms import ContactForm
+
+
+class ContactView(CreateView):
+
+    model = Contact
+    form_class = ContactForm
+    success_url = "/"
+    template_name = "main/contact.html"
+
+    def form_valid(self, form):
+        form.save()
+        send(form.instance.email)
+        return super().form_valid(form)
