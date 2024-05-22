659f209c52f80d2c6b79676f5050845abe08b363
K0ndrich <123kondrich@gmail.com>
2024-05-22 11:40:29 +0300

    edited

--- send_email/main/tasks.py
+++ send_email/main/tasks.py
@@ -7,6 +7,6 @@ from .service import send
 # @app.task декоратор указыват что ето фнукция которую будет выполянть celery
 @app.task
 def send_spam_email(a=5):
-    
+
     print(a)
     # send(user_email)
